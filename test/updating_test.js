034ab32108c3e2ce0f903d86470ac3303e0fb139
Abhishek Choudhary <acabhishek942@gmail.com>
2018-07-16 23:52:36 +0530

    test for updating records

---
+++ test/updating_test.js
@@ -0,0 +1,35 @@
+const mocha =  require('mocha');
+const assert = require('assert');
+const workExperience = require('../models/work_experience');
+
+// Describe test
+describe('Updating record', function(){
+
+    var work;
+    beforeEach(function(done){
+        work = new workExperience({
+            desingnation: 'test desingnation',
+            organization: 'test org',
+            location: 'test Location',
+            startDate: Date().now,
+            endDate: Date().now,
+            tasks: [
+                'test task 1', 'test task2'
+            ]
+        });
+        work.save().then(function(){
+            assert(work.isNew === false);
+            done();
+        });
+    })
+
+    // create tests
+    it('Update a record from the DB', function(done){
+        workExperience.findOneAndUpdate({desingnation: 'test desingnation'}, {desingnation: 'updated desingnation'}).then(function(){
+            workExperience.findOne({desingnation: 'updated desingnation'}).then(function(result){
+                assert(result.desingnation==='updated desingnation');
+                done();
+            })
+        })
+    });
+})
