e71433dfe427d42459ab1b04259db7dc70d26574
Abhishek Choudhary <acabhishek942@gmail.com>
2018-07-16 16:57:27 +0530

    writing a unit test for testing save functionality

---
+++ test/saving_test.js
@@ -0,0 +1,25 @@
+const mocha =  require('mocha');
+const assert = require('assert');
+const workExperience = require('../models/work_experience');
+
+// Describe test
+describe('Saving records', function(){
+
+    // create tests
+    it('Saves a record to the DB', function(done){
+        var work = new workExperience({
+            desingnation: 'test desingnation',
+            organization: 'test org',
+            location: 'test Location',
+            startDate: Date().now,
+            endDate: Date().now,
+            tasks: [
+                'test task 1', 'test task2'
+            ]
+        });
+        work.save().then(function(){
+            assert(work.isNew === false);
+            done();
+        });
+    });
+})
